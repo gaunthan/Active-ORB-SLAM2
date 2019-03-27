a4d24e65fd530164e8696f96245b95668b17fced
gaunthan <gaunthanhuang@foxmail.com>
2019-03-27 16:45:47 +0800

    Fix bug: use proper camera configuration for Kinect V1

--- run_with_kinect_v1.sh
+++ run_with_kinect_v1.sh
@@ -1,2 +1,2 @@
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:`pwd`/Examples/ROS/ORB_SLAM2
-rosrun ORB_SLAM2 RGBD Vocabulary/ORBvoc.bin Examples/RGB-D/kinect.yaml
+rosrun ORB_SLAM2 RGBD Vocabulary/ORBvoc.bin Examples/RGB-D/TUM1.yaml
